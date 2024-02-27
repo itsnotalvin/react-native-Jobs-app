9c269df516a6628ebc5a8d0b3f778633a6d7cc54
Alvin Yip <alvin.y97@gmail.com>
2024-02-27 23:40:52 +1100

    fixed react child object issue

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -15,7 +15,12 @@ const Popularjobs = () => {
     num_pages: '1'
   })
 
-  console.log(data);
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
 
   return (
     <View style={styles.container}>
@@ -37,6 +42,8 @@ const Popularjobs = () => {
             renderItem={({ item }) => (
               <PopularJobCard 
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={item => item.job_id}
